81727c33bf87fae422ec165d290e9896787a0f63
Kyle Norton <kylebradleynorton@gmail.com>
2019-09-05 20:30:44 -0600

    next version

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ massive(process.env.DATABASE_URL)
         app.post('/device/entry', controller.addDevice);
         app.post('/tape/entry', controller.addTape);
         app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
         })
 
         app.listen(5000, () => console.log('listening on 5000'))
